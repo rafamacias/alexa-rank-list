0c96cab663bc031af128e459d462a2b80789ebd5
Rafa Macias <email@rafamacias.com>
2018-10-04 18:16:38 +0900

    Added settings file and pull the variables from there

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import ExcelFile from './lib/ExcelFile.mjs'
+import settings from './settings.mjs'
 
-const excelFile = new ExcelFile()
+const excelFile = new ExcelFile(settings)
 excelFile.process()
 
---
+++ settings.mjs
@@ -0,0 +1,9 @@
+export default {
+    inputFile: 'sample.xlsx',       // default 'sample.xlsx'
+    outputFile: 'new.xlsx',         // default 'new.xlsx'
+    sheet: 1,                       // default 1
+    cellDomain: 'B',                 // default 'B'
+    cellGlobalRank: 'E',             // default 'C'
+    cellLocalRank: 'F',        // default 'D'
+    cellCountry: 'I'          // default 'E'
+}
