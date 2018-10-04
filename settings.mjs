f2906c8fe1b370dbb69ffcc0041036b63663e1ae
Rafa Macias <email@rafamacias.com>
2018-10-04 18:42:31 +0900

    added a max num of rows to find/edit

--- settings.mjs
+++ settings.mjs
@@ -1,9 +1,10 @@
 export default {
-    inputFile: 'sample.xlsx',       // default 'sample.xlsx'
+    inputFile: 'sample3.xlsx',      // default 'sample.xlsx'
     outputFile: 'new.xlsx',         // default 'new.xlsx'
     sheet: 1,                       // default 1
-    cellDomain: 'B',                 // default 'B'
-    cellGlobalRank: 'E',             // default 'C'
-    cellLocalRank: 'F',        // default 'D'
-    cellCountry: 'I'          // default 'E'
+    cellDomain: 'D',                // default 'B'
+    cellGlobalRank: 'I',            // default 'C'
+    cellLocalRank: 'J',             // default 'D'
+    cellCountry: 'K',              // default 'E'
+    maxRows: 5                    // default 300
 }
