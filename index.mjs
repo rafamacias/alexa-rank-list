b3b23a64fb58141cb21100890c1126bd6dd0986a
Rafa Macias <email@rafamacias.com>
2018-10-11 14:09:36 +0900

    added support for CSV

--- index.mjs
+++ index.mjs
@@ -1,6 +1,20 @@
 import ExcelFile from './lib/ExcelFile.mjs'
+import CSVFile from './lib/CSVFile.mjs'
+
+import Alexa from './lib/AlexaRank.mjs'
 import settings from './settings.mjs'
 
-const excelFile = new ExcelFile(settings)
-excelFile.process()
+let sourceFile;
+// TODO: Check that inputfile and column exists in Settings and its values or Throw an Error and exit
 
+if (settings.inputFile.endsWith('.csv')) {
+    sourceFile = new CSVFile(settings, Alexa)
+
+} else if (settings.inputFile.endsWith('.xlsx')) {
+    sourceFile = new ExcelFile(settings, Alexa)
+
+} else {
+    throw new Error('File type not supported. Now only .csv & .xlsx. Modify the file "settings.mjs" ')
+}
+
+sourceFile.process()
--- settings.mjs
+++ settings.mjs
@@ -1,10 +1,16 @@
 export default {
-    inputFile: 'sample3.xlsx',      // default 'sample.xlsx'
-    outputFile: 'new.xlsx',         // default 'new.xlsx'
+    inputFile: 'data/in/sample.csv',         // Mandatory
+    column: {
+        domain: 'D',                // Mandatory
+        globalRank: 'I',            // Mandatory
+        localRank: 'J',             // Mandatory
+        country: 'K',               // Mandatory
+    },
+    outputFile: 'data/out/new.csv',          // default 'output.csv'
     sheet: 1,                       // default 1
-    cellDomain: 'D',                // default 'B'
-    cellGlobalRank: 'I',            // default 'C'
-    cellLocalRank: 'J',             // default 'D'
-    cellCountry: 'K',              // default 'E'
-    maxRows: 5                    // default 300
+    startingRow: 0,              // default 0
+    amountRows: 1000000,            // default 10000
+    rowsAfterToDelay: 500,          // default 100
+    delayAfterRows: 1000 * 60 * 5,           // default 1000
+    delayBetweenRows: 100             // default 0 ms
 }
