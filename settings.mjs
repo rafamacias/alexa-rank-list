c765b5848f77ac08f949a60e0ae6b98bdf9a11f4
Rafa Macias <email@rafamacias.com>
2018-10-12 15:32:34 +0900

    cleanup files

--- settings.mjs
+++ settings.mjs
@@ -1,16 +1,16 @@
 export default {
-    inputFile: 'data/in/sample.csv',         // Mandatory
+    inputFile: 'data/in/companiesWithDomainNoAlexa.csv',         // Mandatory
     column: {
-        domain: 'D',                // Mandatory
-        globalRank: 'I',            // Mandatory
-        localRank: 'J',             // Mandatory
-        country: 'K',               // Mandatory
+        domain: 'Company Domain Name', 
+        globalRank: 'Company Website Global Alexa Rank',
+        localRank: 'Company Website Local Alexa Rank',
+        country: 'Country',
     },
     outputFile: 'data/out/new.csv',          // default 'output.csv'
     sheet: 1,                       // default 1
     startingRow: 0,              // default 0
     amountRows: 1000000,            // default 10000
     rowsAfterToDelay: 500,          // default 100
-    delayAfterRows: 1000 * 60 * 5,           // default 1000
-    delayBetweenRows: 100             // default 0 ms
+    delayAfterRows: 1000 * 60 * 3,           // default 1000
+    delayBetweenRows: 500             // default 0 ms
 }
